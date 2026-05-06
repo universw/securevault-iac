e83f8c9bde47a077c95f29c95264bb96d1d7e500
universw <jmiaakgasxomhprmut@gmail.com>
2026-05-06 17:26:53 +0700

    s

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -57,9 +57,9 @@ resource "aws_s3_bucket_cors_configuration" "securevault_files_cors" {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "PUT", "POST", "DELETE", "HEAD"]
     allowed_origins = [
-  "http://localhost:5173",
-  "https://securevault-iac.vercel.app"
-]
+      "http://localhost:5173",
+      "https://securevault-iac.vercel.app"
+    ]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
